5084e4b74c99ab7649377bdc754cadbae36da14c
brenopolanski <breno.polanski@gmail.com>
2018-10-22 16:54:11 -0300

    Update App

--- app-sample/src/App.js
+++ app-sample/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import OODTSample from './components/react-oodt-plugin-sample';
+// import OODTPluginSample from './components/react-oodt-plugin-sample';
+import OODTPluginSample from 'react-oodt-plugin-sample';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <OODTSample productId="ce4380c5-d0d2-11e8-89ca-971c29fc9f21" />
+        <OODTPluginSample productId="ce4380c5-d0d2-11e8-89ca-971c29fc9f21" />
       </div>
     );
   }
